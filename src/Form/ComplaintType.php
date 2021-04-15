90fc8cc5065f0397a5be4cb8033a9d643d96eb33
Sabghine <sabrine.mokhtar@esprit.tn>
2021-04-15 22:55:14 +0100

    Input control + Twig modifications + Implemented template

--- src/Form/ComplaintType.php
+++ src/Form/ComplaintType.php
@@ -15,12 +15,7 @@ class ComplaintType extends AbstractType
             ->add('object')
             ->add('description')
             ->add('status')
-            ->add('createdDate')
-            ->add('updatedDate')
-            ->add('archivedDate')
-            ->add('createdBy')
-            ->add('updatedBy')
-            ->add('archivedBy')
+
         ;
     }
 
