2ee144077809106370bf2f5bea38d43c7518c6ec
MohsniNour <nourelhouda.mohsni@esprit.tn>
2021-04-16 01:13:37 +0100

    updated activity/workDone add file + view + added back template

--- src/Controller/WorkDoneController.php
+++ src/Controller/WorkDoneController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Activity;
 use App\Entity\WorkDone;
 use App\Form\WorkDoneType;
 use App\Repository\WorkDoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
- * @Route("/work/done")
+ * @Route("/workDone")
  */
 class WorkDoneController extends AbstractController
 {
@@ -26,15 +30,41 @@ class WorkDoneController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="work_done_new", methods={"GET","POST"})
+     * @Route("/new/{idActivity}", name="work_done_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $workDone = new WorkDone();
         $form = $this->createForm(WorkDoneType::class, $workDone);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $brochureFile */
+            $brochureFile = $form->get('workFile')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $workDone->setWorkFile($newFilename);
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($workDone);
             $entityManager->flush();
